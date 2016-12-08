b03796f6e6209d82c79f59910d025435ebd839bd
jc00721 <jc00721@surrey.ac.uk>
2016-12-08 17:23:58 +0000

    Added index to artists

--- db/migrate/20161208171953_create_artists.rb
+++ db/migrate/20161208171953_create_artists.rb
@@ -1,9 +1,9 @@
 class CreateArtists < ActiveRecord::Migration
   def change
     create_table :artists do |t|
-      t.text :name
-      t.date :dob
-      t.text :label
+      t.text :name, null: false
+      t.date :dob, null: false
+      t.text :label, null: false
 
       t.timestamps null: false
     end
