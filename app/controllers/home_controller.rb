6e39c72b81d39c3a99f4ea2cc87cf8361b3eb69b
James Cooper <jc00721@surrey.ac.uk>
2016-12-12 14:04:11 +0000

    Replace mailer validation with more user friendly alternative

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,12 +10,8 @@ class HomeController < ApplicationController
     email = params[:email]
     subject = params[:subject]
     message = params[:message]
-    if email.blank?
-      flash[:alert] = I18n.t("home.request_contact.no_email")
-    else
-      ContactMailer.contact_email(email, name, subject, message).deliver_now
-      flash[:notice] = I18n.t("home.request_contact.enail_sent")
-    end
-   redirect_to root_path
+    ContactMailer.contact_email(email, name, subject, message).deliver_now
+    flash[:notice] = I18n.t("home.request_contact.email_sent")
+    redirect_to root_path
   end
 end
